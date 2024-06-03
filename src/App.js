8cb4df84c5ffb37fc9f8b9300268e78bd5367e20
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2024-06-03 23:31:33 +0300

    fix register styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Register from './Components/Register/Register';
-import Layout from './Components/Layout/Layout';
+import Register from "./Components/Register/Register";
+import Layout from "./Components/Layout/Layout";
+import Login from "./Components/Login/Login";
 
 const router = createBrowserRouter([
-  {path: '/', element: <Layout />, children: [
-    {path: 'register', element: <Register />},
-  ]},
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
-
+  return <RouterProvider router={router} />;
 }
 
 export default App;
